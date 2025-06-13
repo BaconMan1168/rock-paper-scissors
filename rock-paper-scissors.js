fd96483aa476e1f077fcca487273bb5bafd2bd47
Daniel <198354403+BaconMan1168@users.noreply.github.com>
2025-06-13 17:19:03 +0800

    Oops forgot to add commits gradually

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,3 +12,68 @@ function getComputerChoice(){
     }
 }
 
+function getHumanChoice(){
+    let choice = prompt("Enter your choice (rock, paper, or scissors)");
+    return choice.toLowerCase();
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    let humanLower = humanChoice.toLowerCase();
+    let computerLower = computerChoice.toLowerCase();
+
+    switch(humanLower){
+        case "rock":
+            if (computerLower === "paper"){
+                console.log("You Lose! " + computerLower + " beats " + humanLower);
+            }
+            else if(computerLower === "scissors"){
+                console.log("You Win! " + humanLower + " beats " + computerLower);
+            }
+            else{
+                console.log("You tie! " + humanLower + " is equal to " + computerLower);
+            }
+            break;
+        
+        case "paper":
+            if (computerLower === "paper"){
+                console.log("You tie! " + humanLower + " is equal to " + computerLower);
+                
+            }
+            else if(computerLower === "scissors"){
+                console.log("You Lose! " + computerLower + " beats " + humanLower);
+            }
+            else{
+                console.log("You Win! " + humanLower + " beats " + computerLower);
+                
+            }
+            break;
+        
+        case "scissors":
+            if (computerLower === "paper"){
+                console.log("You Win! " + humanLower + " beats " + computerLower);
+                
+            }
+            else if(computerLower === "scissors"){
+                console.log("You tie! " + humanLower + " is equal to " + computerLower);
+                
+            }
+            else{
+                console.log("You Lose! " + computerLower + " beats " + humanLower);
+                
+            }
+            break;
+    }
+}
+
+
+function playGame(){
+    for (let i = 0; i < 5; i++){
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+}
+
+
+playGame();
